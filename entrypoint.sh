8fd284e272f93eba1d24c0f2fc525df4aa44d642
Christian Meiners <chrischan-services@aikq.de>
2025-02-20 18:38:00 +0100

    Other approach for let the container running in debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Check if the DEBUG environment variable is set
 if [ "$DEBUG" == "true" ]; then
     echo "Debug mode activated. Dropping into a shell."
-    exec /bin/bash  # Start an interactive shell for debugging
+    tail -f /dev/null
 else
     echo "Running internxt_script.sh..."
     exec /usr/local/bin/internxt_script.sh  # Replace with the actual path to the internxt_script.sh
