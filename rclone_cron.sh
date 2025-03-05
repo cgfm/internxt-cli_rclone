f219a59ce36b0b201dd586231813b16d270e62c3
Christian Meiners <chrischan-services@aikq.de>
2025-03-05 21:58:04 +0100

    Create rclone_cron.sh

---
+++ rclone_cron.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# rclone_cron.sh
+
+set -e
+
+# Log directory
+LOG_DIR="/config/log"
+
+# Set RCLONE_CONFIG if not set
+if [ -z "$RCLONE_CONFIG" ]; then
+    RCLONE_CONFIG = "/config/rclone.conf"
+fi
+
+# Prepare the CRON_COMMAND
+if [ -z "$CRON_COMMAND" ]; then
+    CRON_COMMAND="rclone sync --create-empty-src-dirs --retries 5 --verbose --config ${RCLONE_CONFIG}"
+fi
+
+# Loop to append remote and local paths to the CRON_COMMAND
+for i in {1..20}; do
+    remote_var="REMOTE_PATH_$i"
+    local_var="LOCAL_PATH_$i"
+    
+    if [ ! -z "${!remote_var}" ] && [ ! -z "${!local_var}" ]; then
+        eval "${CRON_COMMAND} ${!local_var} ${!remote_var} --log-file $LOG_DIR/rclone.log --log-format date,time,UTC"
+    fi
+done
