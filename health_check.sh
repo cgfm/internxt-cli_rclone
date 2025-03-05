a6e0a21785416a7bc3acef84bb8a1c63d7058e9a
Christian Meiners <chrischan-services@aikq.de>
2025-03-05 07:00:11 +0100

    Update health_check.sh

--- health_check.sh
+++ health_check.sh
@@ -14,16 +14,25 @@ if [[ "$WEBDAV_STATUS" != *"online"* ]]; then
     error_exit "Internxt WebDAV server is not running. Status: $WEBDAV_STATUS"
 fi
 
-# Determine whether to use HTTP or HTTPS for rclone Web GUI
-if [ -n "$RCLONE_SSL_CERT" ] && [ -n "$RCLONE_SSL_KEY" ]; then
-    WEB_GUI_URL="https://localhost:$RCLONE_WEB_GUI_PORT"
-else
-    WEB_GUI_URL="http://localhost:$RCLONE_WEB_GUI_PORT"
-fi
+# check rClone webgui if it should be served 
+if [ "${RCLONE_WEB_GUI_SERVE:-true}" = "true" ]; then
+   
+    # Determine whether to use HTTP or HTTPS for rclone Web GUI
+    if [ -n "$RCLONE_SSL_CERT" ] && [ -n "$RCLONE_SSL_KEY" ]; then
+        WEB_GUI_URL="https://localhost:$RCLONE_WEB_GUI_PORT"
+    else
+        WEB_GUI_URL="http://localhost:$RCLONE_WEB_GUI_PORT"
+    fi
 
-# Check if the rclone Web GUI is running and accessible
-if ! curl --connect-timeout 5 -s --head --user "$RCLONE_GUI_USER:$RCLONE_GUI_PASS" "$WEB_GUI_URL" | grep -q "200 OK"; then
-    error_exit "rclone Web GUI is not accessible."
+    # Check if the rclone Web GUI is running and accessible if both user and pass are set check with credentials 
+    if [ -n "$RCLONE_WEB_GUI_USER" ] && [ -n "$RCLONE_WEB_GUI_PASS" ]; then
+        if ! curl --connect-timeout 5 -s --head --user "$RCLONE_GUI_USER:$RCLONE_GUI_PASS" "$WEB_GUI_URL" | grep -q "200 OK"; then
+            error_exit "rclone Web GUI is not accessible."
+        fi
+    else
+        if ! curl --connect-timeout 5 -s --head "$WEB_GUI_URL" | grep -q "200 OK"; then
+            error_exit "rclone Web GUI is not accessible."
+        fi
 fi
 
 # Check if cron jobs are enabled and running only if CRON_SCHEDULE is not empty
