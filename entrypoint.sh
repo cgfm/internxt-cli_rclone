f5b7ac3c3a6abc00aad8b3eda62b97b7d30ee49b
Christian Meiners <chrischan-services@aikq.de>
2025-03-12 11:04:44 +0100

    typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,11 @@ log_debug() {
     # Check if LOG_LEVEL is set to "debug" or higher
     if [ "$LOG_LEVEL" = "fine" ]; then
         echo "[FINE]: $message"
-    elif { [ "$LOG_LEVEL" = "fine" ] || [ "$LOG_LEVEL" = "debug" ] } && [ "$level" = "debug" ]; then
+    elif [ [ "$LOG_LEVEL" = "fine" ] || [ "$LOG_LEVEL" = "debug" ] ] && [ "$level" = "debug" ]; then
         echo "[LOG_LEVEL]: $message"
-    elif { [ "$LOG_LEVEL" = "fine" ] || [ "$LOG_LEVEL" = "debug" ] || [ "$LOG_LEVEL" = "info" ] } && [ "$level" = "info" ]; then
+    elif [ [ "$LOG_LEVEL" = "fine" ] || [ "$LOG_LEVEL" = "debug" ] || [ "$LOG_LEVEL" = "info" ] ]  && [ "$level" = "info" ]; then
         echo "[INFO]: $message"
-    elif { [ "$LOG_LEVEL" = "fine" ] || [ "$LOG_LEVEL" = "debug" ] || [ "$LOG_LEVEL" = "info" ] || [ "$LOG_LEVEL" = "error" ] } && [ "$level" = "error" ]; then
+    elif [ [ "$LOG_LEVEL" = "fine" ] || [ "$LOG_LEVEL" = "debug" ] || [ "$LOG_LEVEL" = "info" ] || [ "$LOG_LEVEL" = "error" ] ] && [ "$level" = "error" ]; then
         echo "[ERROR]: $message"
     fi
 }
